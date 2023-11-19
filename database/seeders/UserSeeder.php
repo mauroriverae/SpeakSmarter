6cfe693668727e1128cf27c91233de85a281cb3a
Mauro Rivera <mauroriverae@gmail.com>
2023-11-19 19:40:17 -0300

    added the jetstream/inertia for sessions & vue  and Spatie component for role management

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+        $admin->assignRole('admin');
+
+        $editor = User::create([
+            'name' => 'editor',
+            'email' => 'editor@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $editor->assignRole('editor');
+    }
+}
---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  methods: {
+    hasPermission(permission) {
+      return this.$page.props.user.permissions.includes(permission);
+    },
+  },
+};
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+            <ApplicationLogo class="block h-12 w-auto" />
+
+            <h1 class="mt-8 text-2xl font-medium text-gray-900">
+                Welcome to your Jetstream application!
+            </h1>
+
+            <p class="mt-6 text-gray-500 leading-relaxed">
+                {{ $page.props }}
+            </p>
+
+            <button v-if="hasPermission('create roles')">Manage Roles</button>
+            <button v-if="hasPermission('create lessons')">Manage Lessons</button>
+
+        </div>
+  
+
+    </div>
+</template>
+
+<style>
+    button {
+        background-color: rgb(34, 67, 255);
+        color: white;
+        border: 1px solid black;
+        border-radius: 15px;
+        padding: 20px;
+        margin: 20px;
+    }
+</style>
